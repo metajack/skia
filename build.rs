c2442d6d54895e8e7b163b13cb1cbb935f054fe4
Vladimir Vukicevic <vladimir@pobox.com>
2016-06-03 16:33:54 -0400

    Update CMake and build.rs for MSVC

--- build.rs
+++ build.rs
@@ -7,22 +7,31 @@ extern crate cmake;
 use std::env;
 
 fn main() {
-    let dst = cmake::Config::new(".").generator("ninja").build_target("all").build();
-    println!("cargo:rustc-link-search=native={}", dst.display());
+    let dst = cmake::Config::new(".")
+        .define("CMAKE_C_COMPILER", "cl.exe")
+        .define("CMAKE_CXX_COMPILER", "cl.exe")
+        .define("CMAKE_LINKER", "C:\\Program Files (x86)\\Microsoft Visual Studio 12.0\\VC\\bin\\amd64\\link.exe")
+        .build();
+    println!("cargo:rustc-link-search=native={}/lib", dst.display());
     println!("cargo:rustc-link-lib=static=skia");
-    println!("cargo:rustc-link-lib=stdc++");
-//    println!("cargo:outdir={}", out_dir);
+    println!("cargo:outdir={}", dst.display());
 
     let target = env::var("TARGET").unwrap();
     if target.contains("unknown-linux-gnu") {
+        println!("cargo:rustc-link-lib=stdc++");
         println!("cargo:rustc-link-lib=bz2");
         println!("cargo:rustc-link-lib=GL");
     } else if target.contains("eabi") {
+        println!("cargo:rustc-link-lib=stdc++");
         println!("cargo:rustc-link-lib=GLESv2");
     } else if target.contains("apple-darwin") {
+        println!("cargo:rustc-link-lib=stdc++");
         println!("cargo:rustc-link-lib=framework=OpenGL");
         println!("cargo:rustc-link-lib=framework=ApplicationServices");
     } else if target.contains("windows") {
+        if target.contains("gnu") {
+            println!("cargo:rustc-link-lib=stdc++");
+        }
         println!("cargo:rustc-link-lib=usp10");
         println!("cargo:rustc-link-lib=ole32");
     }
